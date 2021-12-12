28de2b88f760a95aaf2ad6eb551ea3b665a7e280
Aram-Jin <dkfka1295@gmail.com>
2021-12-12 11:47:21 +0900

    .

--- keras/keras24_dacon_wine.py
+++ keras/keras24_dacon_wine.py
@@ -1,10 +1,15 @@
-from enum import auto
-import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
+import numpy as np, pandas as pd, datetime, matplotlib.pyplot as plt, seaborn as sns  
+from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler, RobustScaler, MaxAbsScaler, OneHotEncoder
+from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, GradientBoostingClassifier
+from sklearn.model_selection import cross_validate
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.metrics import classification_report,confusion_matrix, accuracy_score, recall_score, precision_score, f1_score, confusion_matrix
 from tensorflow.keras.models import Sequential, Model, load_model
 from tensorflow.keras.layers import Dense, Input, Dropout
-from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, MaxAbsScaler
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+#from graphviz import Source
 
 #1. 데이터
 path = "../_data/dacon/wine/"
@@ -26,12 +31,11 @@ submit_file = pd.read_csv(path + 'sample_submission.csv')
 #       'quality'],
 #      dtype='object')
 
-x = train.drop(['id', 'quality'], axis=1)   # axis값을 1을 주면 열(세로) 값을 0을주면 행(가로) 삭제함. default값은 0
-test_file = test_file.drop(['id'], axis=1)
 y = train['quality']
-
-from sklearn import preprocessing
-from sklearn.preprocessing import LabelEncoder
+x = train.drop(['id', 'quality'], axis=1)  
+x = x.drop(['sulphates'], axis=1) # 'citric acid', 'pH',
+test_file = test_file.drop(['id', 'sulphates'], axis=1) # ,  'sulphates' 'citric acid', 'pH',
+y = y.to_numpy()
 
 le = LabelEncoder()
 label = x['type']
@@ -44,55 +48,130 @@ test_file['type'] = le.transform(label2)
 
 #print(test_file['type'])       # testfile의 type열의 값이 0,1로 바뀌어있는지 확인해봄.
 
-print(np.unique(y))  # [4 5 6 7 8]
+#plt.figure(figsize=(10,10))
+#sns.heatmap(data=x.columns.corr(), square=True, annot=True, cbar=True) 
+#plt.show()
 
-y = pd.get_dummies(y)  # pd.get_dummies 처리 : 결측값을 제외하고 0과 1로 구성된 더미값이 만들어진다. 
-# 결측값 처리(dummy_na = True 옵션) : Nan을 생성하여 결측값도 인코딩하여 처리해준다.
-# y = pd.get_dummies(y, drop_first=True) : N-1개의 열을 생성
-#print(y.shape)   # (3231, 5) -> y가 5개의 열로 바뀜(quality가 원핫인코딩으로 0,1,2,3,4 다섯개의 열이 됨)
+#print(np.unique(y))  # [4 5 6 7 8] 
+
+#y = np.array(y).reshape(-1,1)
+#enc= OneHotEncoder()   #[0. 0. 1. 0. 0.]
+#enc.fit(y)
+#y = enc.transform(y).toarray()
+#x = x.to_numpy()
+
+#y = pd.get_dummies(y)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y,
-                                                    train_size=0.8, shuffle=True, random_state=49)
+                                                    train_size=0.8, shuffle=True, random_state=61)
 
-#scaler = MinMaxScaler()
+
+#model2 = RandomForestClassifier(random_state=66)
+#model2.fit(x_train, y_train)
+#y_pred2 = model2.predict(x_test)
+
+#print(y_pred2)
+
+
+'''
+rf = RandomForestClassifier(n_jobs = -1 , random_state =66)
+score = cross_validate(rf, x_train, y_train, return_train_score=True, n_jobs= -1)
+print(np.mean(score['train_score']), np.mean(score['test_score']))  # 1.0 0.46322102769406087
+
+rf.fit(x_train, y_train)
+print(rf.feature_importances_)   # [0.07706013 0.10137848 0.0758608  0.08726533 0.08503215 0.08534749
+                                 #  0.08959861 0.10268387 0.07732534 0.08378797 0.13163588 0.00302395]
+
+dt = DecisionTreeClassifier(random_state =66)
+dt.fit(x_train, y_train)
+y_pred = dt.predict(x_test)
+
+decision_acc = accuracy_score(y_test, y_pred)
+print('의사결정나무의 정확도: ',decision_acc)   #  0.5486862442040186
+
+decision_report = classification_report(y_test, y_pred)
+print(decision_report)
+
+decision_matrix = confusion_matrix(y_test, y_pred)
+print(decision_matrix)
+#pred_train = dt.predict(x_train)
+#pred_test = dt.predict(x_test)
+
+#print(dt.feature_importances_)
+
+rf = RandomForestClassifier(oob_score= True, n_jobs = -1 , random_state =66)
+rf.fit(x_train, y_train)
+print(rf.oob_score_)   # 0.8530959752321982
+
+es = ExtraTreesClassifier(n_jobs = -1 , random_state =66)
+score = cross_validate(es, x_train, y_train, return_train_score=True, n_jobs= -1)
+print(np.mean(score['train_score']), np.mean(score['test_score']))   # 1.0 0.46361912044742326
+
+#gb = GradientBoostingClassifier(n_estimators=500, learning_rate=0.2, random_state=66)
+#score = cross_validate(gb, x_train, y_train, return_train_score=True, n_jobs= -1)
+#print(np.mean(score['train_score']), np.mean(score['test_score'])) 
+
+#print(y.shape)  # (3231, 5)
+#print(x.shape)  # (3231, 9)
+'''
+#confusion_matrix(y_train, pred_train)
+#confusion_matrix(y_test, pred_test)
+
+#accuracy_score(y_train, pred_train), accuracy_score(y_test, pred_test)
+#recall_score(y_test, pred_test)
+#precision_score(y_test, pred_test)
+
+#eg = export_graphviz(dt, out_file=None, feature_names=x.columns, class_names=['White','Red'], rounded=True,filled=True)
+
+scaler = MinMaxScaler()
 #scaler = StandardScaler()
 #scaler = RobustScaler()
-scaler = MaxAbsScaler()
+#scaler = MaxAbsScaler()
 scaler.fit(x_train)
 x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
 test_file = scaler.transform(test_file)
 
+model2 = RandomForestClassifier(oob_score= True, n_estimators=15000, random_state=61)
+model2.fit(x_train, y_train)
+y_pred2 = model2.predict(test_file)
+
+print(model2.oob_score_)
+print(y_pred2)
+
+submit_file['quality'] = y_pred2
+submit_file.to_csv(path+'subfile1.csv', index=False)
+      
+#submission['quality'] = y_pred2
+#submission.to_csv("dacon_wine.csv", index=False)
+
+
+'''
 #2. 모델구성
-input1 = Input(shape=(12,))
-dense1 = Dense(60, activation='relu')(input1)
-drop1 = Dropout(0.2)(dense1)
-dense2 = Dense(40, activation='relu')(drop1)
-drop2 = Dropout(0.2)(dense2)
-dense3 = Dense(20, activation='relu')(drop2)
-dense4 = Dense(10, activation='relu')(dense3)
-output1 = Dense(5, activation='softmax')(dense4)
-model = Model(inputs=input1, outputs=output1)
-model.summary()
+model = Sequential() 
+model.add(Dense(64, input_dim=12))
+model.add(Dropout(0.2))  
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.2))  
+model.add(Dense(16, activation='relu'))
+model.add(Dropout(0.2))  
+model.add(Dense(8, activation='relu'))
+model.add(Dropout(0.1))  
+model.add(Dense(5, activation='softmax'))
+#model.summary()
 
 #3. 컴파일, 훈련
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
 
-###################################################################################################################################
-import datetime
 date = datetime.datetime.now()
-datetime_spot = date.strftime("%m%d_%H%M")  # 1206_0456
-#print(datetime_spot)   
-filepath = './_dacon_wine/'                 # ' ' -> 문자열 형태
-filename = '{epoch:04d}-{val_accuracy:.4f}.hdf5'     # epoch:04d -> 네자리수;천단위,  val_loss:.4f -> 소수점뒤로 0네개  #2500-0.3724
+datetime_spot = date.strftime("%m%d_%H%M")  
+filepath = './_dacon_wine/'                
+filename = '{epoch:04d}-{val_accuracy:.4f}.hdf5'     
 model_path = "".join([filepath, 'dacon_wine_', datetime_spot, '_', filename])
-####################################################################################################################################
 
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-
-es = EarlyStopping(monitor='val_loss', patience=10000, mode='min', verbose=1, restore_best_weights=True)
-mcp = ModelCheckpoint(monitor='val_accuracy', mode='max', verbose=1, save_best_only=True, filepath= model_path)
-model.fit(x_train, y_train, epochs=5000000, batch_size=100, validation_split=0.2, callbacks=[es, mcp])
+Es = EarlyStopping(monitor='val_accuracy', patience=800, mode='max', verbose=1, restore_best_weights=True)
+mcp = ModelCheckpoint(monitor='val_loss', mode='min', verbose=1, save_best_only=True, filepath= model_path)
+hist = model.fit(x_train, y_train, epochs=50000, batch_size=50, validation_split=0.2, callbacks=[Es, mcp])
 
 model.save("./_save/keras24_3_save_model.h5") 
 
@@ -101,15 +180,10 @@ loss = model.evaluate(x_test, y_test)
 print('loss : ',loss[0])
 print('accuracy : ', loss[1])
 
-
-########################### 제출용 제작 ################################
+##################################### 제출용 제작 #########################################
 results = model.predict(test_file)
 
-#print(results)
-
 results_int = np.argmax(results, axis=1).reshape(-1,1) + 4
-
-#print(results_int)
 
 submit_file['quality'] = results_int
 
@@ -119,6 +193,22 @@ acc = str(round(loss[1],4)).replace(".","_")
 submit_file.to_csv(path +f"result/accuracy_{acc}.csv", index=False)
 
 
+plt.plot(hist.history['accuracy'])
+plt.plot(hist.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+'''
 
 '''
 Epoch 00565: val_accuracy did not improve from 0.54739
---
+++ keras/kkkkk.py
@@ -0,0 +1,80 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder, OneHotEncoder
+from sklearn.ensemble import VotingClassifier
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, GradientBoostingClassifier
+from sklearn.metrics import accuracy_score
+
+#1 데이터
+path = "D:\\_data\\dacon\\wine\\" 
+train = pd.read_csv(path +"train.csv")
+test_file = pd.read_csv(path + "test.csv") 
+submission = pd.read_csv(path+"sample_Submission.csv")
+
+y = train['quality']
+x = train.drop(['id','quality'], axis =1)
+x = x.drop(['citric acid','pH','sulphates','total sulfur dioxide'],axis =1)
+test_file =test_file.drop(['id','citric acid','pH','sulphates','total sulfur dioxide'],axis =1)
+le = LabelEncoder()
+le.fit(train['type'])
+x['type'] = le.transform(train['type'])
+
+le.fit(test_file['type'])
+test_file['type'] = le.transform(test_file['type'])
+
+y = y.to_numpy()
+x = x.to_numpy()
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, 
+         train_size = 0.8, shuffle = True, random_state = 66) #455.2 /114
+
+scaler = MinMaxScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+model1 = RandomForestClassifier(oob_score= True, bootstrap=True, class_weight=None, criterion='gini',
+            max_depth=None, max_features='auto', max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None,min_samples_leaf=1, min_samples_split=2,
+            min_weight_fraction_leaf=0.0, n_estimators= 2500, n_jobs=None, verbose=0, warm_start=False, random_state=61)
+
+model2 = GradientBoostingClassifier(n_estimators = 750,random_state=66)
+
+model3 = ExtraTreesClassifier(n_estimators = 750,random_state =66)
+
+from sklearn.experimental import enable_hist_gradient_boosting
+from sklearn.ensemble import HistGradientBoostingClassifier
+model4 = HistGradientBoostingClassifier(random_state =66)
+
+
+#from lightgbm import LGBMClassifier
+#model5 = LGBMClassifier(random_state =66)
+
+
+voting_model = VotingClassifier(estimators=[ ('RandomForestClassifier', model1), ('GradientBoostingClassifier', model2)
+                                            ,('ExtraTreesClassifier', model3),('HistGradientBoostingClassifier', model4)], voting='hard')
+
+classifiers = [model1, model2,model3,model4,model5]
+
+for classifier in classifiers:
+    classifier.fit(x_train, y_train)
+    pred = classifier.predict(x_test)
+    class_name = classifier.__class__.__name__
+    print("============== " + class_name + " ==================")
+    num = accuracy_score(y_test, pred)
+    print('{0} 정확도: {1}'.format(class_name, num))
+    y_pred_ = classifier.predict(test_file)
+    submission['quality'] = y_pred_
+    submission.to_csv(str(num) +"_" + class_name + "_dacon_wine_vote.csv", index=False)
+   
+
+voting_model.fit(x_train, y_train)
+pred = voting_model.predict(x_test)
+
+print('===================== 보팅 분류기 ========================')
+num = str(accuracy_score(y_test, pred))
+print('{0} 정확도: {1}'.format(class_name, num))
+
+y_pred_ = voting_model.predict(test_file)
+
+submission['quality'] = y_pred_
+submission.to_csv(num + "_dacon_wine.csv", index=False)
