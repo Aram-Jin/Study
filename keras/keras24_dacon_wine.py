0bff92427e3a3a709b76e58b10c1fb0776d665aa
Aram-Jin <dkfka1295@gmail.com>
2021-12-12 21:05:18 +0900

    .

--- keras/keras24_dacon_wine.py
+++ keras/keras24_dacon_wine.py
@@ -241,3 +241,52 @@ accuracy :  0.5765069723129272
 loss :  0.995951235294342
 accuracy :  0.5780525207519531
 '''
+
+'''
+
+path = "../_data/dacon/wine/"
+train = pd.read_csv(path + 'train.csv')
+
+test_file = pd.read_csv(path + 'test.csv')
+
+submit_file = pd.read_csv(path + 'sample_submission.csv')  
+
+y = train['quality']
+x = train.drop(['id', 'quality'], axis=1)  
+
+test_file = test_file.drop(['id'], axis=1) # ,  'sulphates' 'citric acid', 'pH',
+y = y.to_numpy()
+
+le = LabelEncoder()
+label = x['type']
+le.fit(label)
+x['type'] = le.transform(label)
+
+label2 = test_file['type']
+le.fit(label2) 
+test_file['type'] = le.transform(label2)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y,
+                                                    train_size=0.8, shuffle=True, random_state=66)
+
+scaler = MinMaxScaler()
+#scaler = StandardScaler()
+#scaler = RobustScaler()
+#scaler = MaxAbsScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+test_file = scaler.transform(test_file)
+
+model2 = RandomForestClassifier(oob_score= True, n_estimators=500, random_state=66)
+model2.fit(x_train, y_train)
+y_pred2 = model2.predict(test_file)
+
+print(model2.oob_score_)
+print(y_pred2)
+
+submit_file['quality'] = y_pred2
+submit_file.to_csv(path+'subfile.csv', index=False)
+
+
+'''
