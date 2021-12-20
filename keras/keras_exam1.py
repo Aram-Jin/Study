28f19bd97ab6d9065abad7e3673165117daf67ab
Aram-Jin <dkfka1295@gmail.com>
2021-12-20 09:23:28 +0900

    .

--- keras/keras_exam1.py
+++ keras/keras_exam1.py
@@ -8,17 +8,17 @@ from tensorflow.keras.layers import LSTM, Dense, Dropout, Input, Concatenate, co
 from tensorflow.keras.callbacks import EarlyStopping
 
 def split_xy5(dataset, time_steps, y_column):
-    x,y = list(), list()  # x와 y에 담겠다.
+    x,y = list(), list()  
     
     for i in range(len(dataset)):
-        x_end_number= i + time_steps    # 몇개로 나누어 줄것인가
-        y_end_number = x_end_number + y_column    # y값 의미
+        x_end_number= i + time_steps    
+        y_end_number = x_end_number + y_column   
         
-        if y_end_number > len(dataset):   # 계속 반복하되 우리가 뽑으려는 결과값이 나오면 break하겠다.
+        if y_end_number > len(dataset):  
             break
         
-        tmp_x = dataset[i:x_end_number, :]  # 4개 행, 모든열
-        tmp_y = dataset[x_end_number: y_end_number, 3]  # 3이니까 자릿값 0,1,2
+        tmp_x = dataset[i:x_end_number, :] 
+        tmp_y = dataset[x_end_number: y_end_number, 3] 
         x.append(tmp_x)
         y.append(tmp_y)   
     return np.array(x),np.array(y)
@@ -29,30 +29,8 @@ path = "../samsung/"
 samsung = pd.read_csv(path +"삼성전자.csv", index_col=0, header = 0, thousands =',', encoding='cp949')
 kiwoom = pd.read_csv(path + '키움증권.csv', index_col=0, header = 0, thousands =',', encoding='cp949')
 
-samsung = samsung.iloc[:893,:].drop(['전일비','Unnamed: 6','등락률', '거래량','금액(백만)', '신용비', '개인', '기관', '외인(수량)', '외국계', '프로그램', '외인비'], axis=1).sort_values(['일자'],ascending=[True]) 
-kiwoom = kiwoom.iloc[:893,:].drop(['전일비','Unnamed: 6','등락률', '거래량','금액(백만)', '신용비', '개인', '기관', '외인(수량)', '외국계', '프로그램', '외인비'], axis=1).sort_values(['일자'],ascending=[True])  
-
-# samsung = samsung.loc[::-1]
-# kiwoom = kiwoom.loc[::-1]
-
-samsung = samsung.values
-kiwoom = kiwoom.values
-
-# high_price = samsung['고가'].values
-# low_price = samsung['저가'].values
-# mid_price = (high_price + low_price) / 2
-
-# print(samsung.index, kiwoom.index)
-
-# day_divided = 50
-# day_length = day_divided + 1
-# day_result = []
-# for i in range(len(mid_price) - day_length):
-#     day_result.append(mid_price[i: i + day_length])
-
-print(samsung)    
-print(kiwoom)
-
+samsung = samsung.iloc[:893,:].drop(['전일비','Unnamed: 6','등락률', '거래량','금액(백만)', '신용비', '개인', '기관', '외인(수량)', '외국계', '프로그램', '외인비'], axis=1).sort_values(['일자'],ascending=[True]).values 
+kiwoom = kiwoom.iloc[:893,:].drop(['전일비','Unnamed: 6','등락률', '거래량','금액(백만)', '신용비', '개인', '기관', '외인(수량)', '외국계', '프로그램', '외인비'], axis=1).sort_values(['일자'],ascending=[True]).values  
 
 # x1 = samsung
 # y1 = samsung['종가']
@@ -63,14 +41,13 @@ print(kiwoom)
 x1, y1 = split_xy5(samsung,5,1)
 x2, y2 = split_xy5(kiwoom,5,1)
 
-# print(x1.shape, y1.shape)  # (888, 5, 4) (888, 1)
-# print(x2.shape, y1.shape)  # (888, 5, 4) (888, 1)
-
 x1 = x1.reshape(888,-1)
 x2 = x2.reshape(888,-1)
 
-# print(x1.shape)
-# print(x2.shape)
+print(x1.shape, y1.shape)  # (888, 20) (888, 1)
+print(x2.shape, y1.shape)  # (888, 20) (888, 1)
+
+# print(samsung.index, kiwoom.index)
 
 x1_train, x1_test, y1_train, y1_test = train_test_split(x1, y1, train_size=0.8, shuffle=True, random_state=66)
 x2_train, x2_test, y2_train, y2_test = train_test_split(x2, y2, train_size=0.8, shuffle=True, random_state=66)
@@ -88,124 +65,72 @@ x1_test = scaler.transform(x1_test).reshape(len(x1_test),5,4)
 x2_train = scaler.fit_transform(x2_train).reshape(len(x2_train),5,4)
 x2_test = scaler.transform(x2_test).reshape(len(x2_test),5,4)
 
-print(x1_train.shape)   # (710, 5, 4)
-print(x2_train.shape)   # (710, 5, 4)
+print(x1_train.shape, x1_test.shape)   # (710, 5, 4)
+print(x2_train.shape, x2_test.shape)   # (710, 5, 4)
+print(y1_train.shape, y2_train.shape, y1_test.shape, y2_test.shape)   # (710, 1) (710, 1) (178, 1) (178, 1)
 
 #2. 모델구성
 
 #2-1 모델1
 input1 = Input(shape=(5,4))
-dense1 = Dense(5, activation='relu', name='dense1')(input1)
-dense2 = Dense(7, activation='relu', name='dense2')(dense1)
-dense3 = Dense(7, activation='relu', name='dense3')(dense2)
-output1 = Dense(7, activation='relu', name='output1')(dense3)
+dense1 = LSTM(16, activation='relu', name='dense1')(input1)
+dense2 = Dense(8, activation='relu', name='dense2')(dense1)
+dense3 = Dense(4, activation='relu', name='dense3')(dense2)
+output1 = Dense(1, activation='relu', name='output1')(dense3)
 
 #2-1 모델2
 input2 = Input(shape=(5,4))
-dense1 = Dense(5, activation='relu', name='dense1')(input1)
-dense2 = Dense(7, activation='relu', name='dense2')(dense1)
-dense3 = Dense(7, activation='relu', name='dense3')(dense2)
-output2 = Dense(7, activation='relu', name='output2')(dense3)
+dense11 = LSTM(16, activation='relu')(input1)
+dense12 = Dense(8, activation='relu')(dense11)
+dense13 = Dense(4, activation='relu')(dense12)
+output2 = Dense(1, activation='relu')(dense13)
 
 merge1 = Concatenate(axis=1)([output1, output2])
 
 #2-3 output모델1
-output21 = Dense(7)(merge1)
-output22 = Dense(11)(output21)
-output23 = Dense(11, activation='relu')(output22)
-last_output1 = Dense(1)(output23)
+output21 = Dense(8, activation='relu')(merge1)
+output22 = Dense(12, activation='relu')(output21)
+output23 = Dense(4, activation='relu')(output22)
+last_output1 = Dense(1, activation='relu')(output23)
 
 #2-4 output모델2
-output31 = Dense(7)(merge1)
-output32 = Dense(11)(output31)
-output33 = Dense(21)(output32)
-output34 = Dense(11, activation='relu')(output33)
-last_output2 = Dense(1)(output34)
+output31 = Dense(8, activation='relu')(merge1)
+output32 = Dense(12, activation='relu')(output31)
+output33 = Dense(4, activation='relu')(output32)
+last_output2 = Dense(1, activation='relu')(output33)
 
 model = Model(inputs=[input1,input2], outputs=[last_output1,last_output2])
+# model.summary()
 
-# print(x_train.shape, x_test.shape, test_file.shape)   # (8708, 8, 1) (2178, 8, 1) (6493, 8, 1)
+#3. 컴파일, 훈련
+from tensorflow.keras.callbacks import EarlyStopping
 
-# def split_x(dataset, size):
-#     list = []
-#     for i in range(len(dataset) - size + 1):
-#         subset = dataset[i : (i + size)]
-#         list.append(subset)
-#     return np.array(list)
+model.compile(loss='mae', optimizer='adam') 
 
-# train = split_x(x1, 5)
-# print(train)
-# print(train.shape)  # (889, 5, 2)
+es = EarlyStopping(monitor='val_loss', patience=100, mode='min', verbose=1, restore_best_weights=True)
 
-# print(samsung.columns)
-# print(kiwoom.columns)
+model.fit([x1_train, x2_train], [y1_train, y2_train], epochs=500, verbose=1, validation_split=0.2, callbacks=[es]) 
 
-# print(samsung)
-# print(kiwoom)
-# print(samsung.shape)  # (893, 2)
-# print(kiwoom.shape)  # (893, 2)
+model.save("./save/keras.exam1.h5")
 
-# range(samsung.shape[0]-1,-1)
-# print(samsung.head())
-# range(samsung.shape[0]-1,-1,::-1)
-# print(samsung.head())
+#4. 평가, 예측
+results = model.evaluate([x1_test, x2_test], [y1_test, y2_test])
+print('loss : ',results)
 
-# # print(samsung.shape)
-# print(samsung.head())
-# print(samsung.tail())
-# print(kiwoom.head())
-# print(kiwoom.tail())
+y1_predict, y2_predict = model.predict([x1_test, x2_test])
+print('삼성전자 종가 : ', y1_predict[-1])
+print('키움증권 종가 : ', y2_predict[-1])
 
+r21 = r2_score(y1_test, y1_predict)
+r22 = r2_score(y2_test, y2_predict)
 
+print('r2_1스코어 : ', r21)
+print('r2_2스코어 : ', r22)
 
-
-# # print(type(samsung))  # <class 'pandas.core.frame.DataFrame'>
-# # print(type(kiwoom))  # <class 'pandas.core.frame.DataFrame'>
-
-# # print(samsung.info())
-# # print(kiwoom.info())
-# # print(samsung.describe())
-# # print(kiwoom.describe())
-
-
-
-# # x1_train, x1_test, x2_train, x2_test, y1_train, y1_test, y2_train, y2_test = train_test_split(x1, x2, y1, y2, train_size=0.8, shuffle=True, random_state=66)
-
-
-
-
-# # plt.figure(figsize=(10,10))
-# # sns.heatmap(data=samsung.columns.corr(), square=True, annot=True, cbar=True) 
-# # plt.show()
-
-# # samsung.pngdf = samsung.loc[samsung['연도']>=1990]
-
-# # plt.figure(figsize=(16, 9))
-# # # sns.lineplot(y=df['종가'], x=df['일자'])
-# # plt.xlabel('time')
-# # plt.ylabel('price')
-
-# # pd.to_datetime(samsung['일자'], format='%Y%m%d')
-# # # 0      2020-01-07
-# # # 1      2020-01-06
-# # # 2      2020-01-03
-# # # 3      2020-01-02
-# # # 4      2019-12-30
-
-# # samsung['일자'] = pd.to_datetime(samsung['일자'], format='%Y%m%d')
-# # samsung['연도'] =samsung['일자'].dt.year
-# # samsung['월'] =samsung['일자'].dt.month
-# # samsung['일'] =samsung['일자'].dt.day
-# # print(samsung.to_datetime)
-
-# # x = train.drop(['datetime', 'casual','registered','count'], axis=1)  
-# # #print(x.shape)  # (10886, 8)
-
-# # y = train['count']
-# # #print(y.shape)  # (10886,)
-
-
-
-
-
-# # test_file = test_file.drop(['datetime'], axis=1)  
+'''
+loss :  [160229.5, 60845.2265625, 99384.2734375]
+삼성전자 종가 :  [0.]
+키움증권 종가 :  [0.]
+r2_1스코어 :  -16.174177281513195
+r2_2스코어 :  -16.905485998919648
+'''
