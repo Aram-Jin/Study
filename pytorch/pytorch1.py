0738fa72b102c9d20efa6bea48d4238175869c31
Aram-Jin <dkfka1295@gmail.com>
2021-12-22 17:24:16 +0900

    .

--- pytorch/pytorch1.py
+++ pytorch/pytorch1.py
@@ -1,8 +1,16 @@
 import torch
+import torchaudio
+import io
+import os
+import requests
+import tarfile
+
+import matplotlib.pyplot as plt
+from IPython.display import Audio, display
 from torch import nn
-from torch.utils.data import 
-import math
-import logging
-from typing import Optional
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda, Compose
+import matplotlib.pyplot as plt
 
-
+#1. 데이터
