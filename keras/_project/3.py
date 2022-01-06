44d636007d6319c0efa3f1dd77a57909a47d7021
Aram-Jin <dkfka1295@gmail.com>
2022-01-06 14:13:34 +0900

    .

--- keras/_project/3.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-from tabulate import tabulate
---
+++ keras/_project/5.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from tabulate import tabulate
+
+code = ['091970']
+# for code in codes: 
+# url = f'http://comp.fnguide.com/SVO2/ASP/SVD_FinanceRatio.asp?pGB=1&gicode=A{code}&cID=&MenuYn=Y&ReportGB=B&NewMenuID=104&stkGb=701'
+# print(url)
+url = f'http://comp.fnguide.com/SVO2/ASP/SVD_FinanceRatio.asp?pGB=1&gicode=A{code}&cID=&MenuYn=Y&ReportGB=B&NewMenuID=104&stkGb=701'
+res = requests.get(url)
+soup = BeautifulSoup(res.content,'lxml')
+table = soup.find_all('table')
+df = pd.read_html(str(table))
+print(tabulate(df[0], headers='keys', tablefmt='psql'))
+
+# dataframe = fs_data(code)
