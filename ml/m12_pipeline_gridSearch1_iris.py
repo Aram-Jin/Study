ef0425c5929ec4ba4054091d3a69577fa6db7ab0
Aram-Jin <dkfka1295@gmail.com>
2022-01-18 16:26:58 +0900

    2022/01/18

--- ml/m12_pipeline_gridSearch1_iris.py
+++ ml/m12_pipeline_gridSearch1_iris.py
@@ -20,6 +20,17 @@ x_train, x_test, y_train, y_test = train_test_split(x, y,
 # x_train = scaler.fit_transform(x_train)
 # x_test = scaler.transform(x_test)
 
+# parameters = [
+#     {'randomforestclassifier__max_depth' : [6, 8, 10]},
+#     {'randomforestclassifier__min_samples_leaf' :[3, 5, 7], 'randomforestclassifier__min_samples_split' : [3, 5, 10]}
+# ]   # (1) make_pipeline사용시, 원래 파라미터는 pipe(RandomForestClassifier)의 파라미터 이므로 파라미터에 명시를 하고 GridSearchCV의 파라미터 형태로 바꿔줌(래핑)
+
+parameters = [
+    {'rF__max_depth' : [6, 8, 10]},
+    {'rF__min_samples_leaf' :[3, 5, 7], 'rF__min_samples_split' : [3, 5, 10]}
+]   # (2) Pipeline사용시, RandomForestClassifier를 "rF"로 명시해주었으므로 rF로 래핑하여 파라미터를 바꿔줌
+
+
 #2. 모델구성
 from sklearn.svm import LinearSVC, SVC
 from sklearn.ensemble import RandomForestClassifier
@@ -28,9 +39,10 @@ from sklearn.pipeline import make_pipeline, Pipeline
 
 #2.모델
 # model = SVC()
-model = make_pipeline(MinMaxScaler(), SVC())   # 스케일러 두번 적용 가능 ; 성능은 잘 모름
-# model = make_pipeline(MinMaxScaler(), SVC())    # 스케일러 두번 적용 가능 ; 성능은 잘 모름 (fit, transform 두개를 이용하여 변환시키는 애들 다 넣을 수 있음)
-# model = Pipeline([("mm", MinMaxScaler()), ("svc", SVC())])   # -> make_pipeline(MinMaxScaler(), SVC()) 와 동일함
+# pipe = make_pipeline(MinMaxScaler(), RandomForestClassifier())   # 스케일러 두번 적용 가능 ; 성능은 잘 모름
+pipe = Pipeline([("mm", MinMaxScaler()), ("rF", RandomForestClassifier())])   # -> make_pipeline(MinMaxScaler(), RandomForestClassifier()) 와 동일함
+
+model = GridSearchCV(pipe, parameters, cv=5, verbose=1)
 
 #3. 훈련
 model.fit(x_train, y_train)
@@ -45,11 +57,6 @@ acc = accuracy_score(y_test, y_predict)
 print("model.score : ", result)
 
 '''
-model.score :  0.9666666666666667
-
-<make_pipeline>
-model.score :  1.0
-
-<스케일러 두번 적용 가능>
+model.score :  0.9333333333333333
 model.score :  0.9333333333333333
 '''
