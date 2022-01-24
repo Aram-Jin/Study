f03f99d10383dab54e853d85a4a923f8578f7451
Aram-Jin <dkfka1295@gmail.com>
2022-01-24 15:33:03 +0900

    2022/01/24

--- ml/m19_bogan3_impute.py
+++ ml/m19_bogan3_impute.py
@@ -14,12 +14,19 @@ print(data)
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import SimpleImputer, KNNImputer, IterativeImputer
 
-# imputer = SimpleImputer(strategy='mean')
+imputer = SimpleImputer(strategy='mean')
 # imputer = SimpleImputer(strategy='median')
 # imputer = SimpleImputer(strategy='most_frequent')  # 많이 사용한 값들로 채우기
-# imputer = SimpleImputer(strategy='constant')
-imputer = SimpleImputer(strategy='constant', fill_value=777)  # 특정값으로 채우기
+# # imputer = SimpleImputer(strategy='constant')
+# imputer = SimpleImputer(strategy='constant', fill_value=777)  # 특정값으로 채우기
 
 imputer.fit(data)
 data2 = imputer.transform(data)
 print(data2)
+
+# # fit에는 dataframe이 들어가는데, 우리는 컬럼만 바꾸고 싶다.
+# # 시리즈를 넣으면 에러가 난다. 처리하시오.
+
+# data = dataframe.loc['a']
+# print(data)
+
---
+++ ml/m20_outliers1.py
@@ -0,0 +1,15 @@
+import  numpy as np
+aaa = np.array([1, 2, -1000, 4, 5, 6, 7, 8, 90, 100, 500])
+
+def outliers(data_out):
+    quartile_1, q2, quartile_3 = np.percentile(data_out, [25,50,75])
+    print("1사분위 : ", quartile_1)
+    print("q2 : ", q2)
+    print("3사분위 : ", quartile_3)
+    iqr = quartile_3 - quartile_1
+    lower_bound = quartile_1 - (iqr * 1.5)
+    upper_bound = quartile_3 + (iqr * 1.5)
+    return np.where((data_out>upper_bound) | (data_out<lower_bound))    
+
+print(outliers(aaa))
+
