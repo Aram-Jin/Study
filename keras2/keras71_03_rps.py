01a93d9d762ce41ebf1ffed1f7264015ca7d588b
Aram Jin <94852416+Aram-Jin@users.noreply.github.com>
2022-03-23 16:22:08 +0900

    Add files via upload

---
+++ keras2/keras71_03_rps.py
@@ -0,0 +1,7 @@
+import numpy as np, time
+from sklearn import datasets
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout
+from tensorflow.keras.callbacks import EarlyStopping
