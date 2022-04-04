7679f8107e4a7d5b57499f455d40a86c9c67ebcb
Aram Jin <94852416+Aram-Jin@users.noreply.github.com>
2022-04-04 10:50:56 +0900

    Add files via upload

--- AE/a06_noise1.py
+++ AE/a06_noise1.py
@@ -8,7 +8,7 @@ x_train = x_train.reshape(60000, 784).astype('float')/255
 x_test = x_test.reshape(10000, 784).astype('float')/255
 
 x_train_noised = x_train + np.random.normal(0, 0.1, size=x_train.shape)
-x_test_noised = x_test + np.random.normal(0, 0.1, size=x_train.shape)
+x_test_noised = x_test + np.random.normal(0, 0.1, size=x_test.shape)
 x_train_noised = np.clip(x_train_noised, a_min=0, a_max=1)
 x_test_noised = np.clip(x_test_noised, a_min=0, a_max=1)
 
@@ -21,6 +21,7 @@ def autoencoder(hidden_layer_size):
     model.add(Dense(units=hidden_layer_size, input_shape=(784,),
                     activation='relu'))
     model.add(Dense(units=784, activation='sigmoid'))
+    return model
     
 model = autoencoder(hidden_layer_size=154)      # pca 95% -> 154
 
@@ -31,4 +32,45 @@ model.fit(x_train_noised, x_train, epochs=10)
 
 output = model.predict(x_test_noised)
 
+from matplotlib import pyplot as plt
+import random
 
+fig, ((ax1, ax2, ax3, ax4, ax5), (ax11, ax12, ax13, ax14, ax15),
+      (ax6, ax7, ax8, ax9, ax10)) = \
+          plt.subplots(3, 5, figsize=(20, 7))
+          
+# 이미지 5개를 무작위로 고른다.
+random_images = random.sample(range(output.shape[0]), 5)
+
+# 원본(입력)이미지를 맨 위에 그린다.
+for i, ax in enumerate([ax1, ax2, ax3, ax4, ax5]):
+    ax.imshow(x_test[random_images[i]].reshape(28, 28), cmap='gray')
+    if i ==0:
+        ax.set_ylabel("INPUT", size=20)
+        ax.grid(False)
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+# 잡음을 넣은 이미지
+for i, ax in enumerate([ax11, ax12, ax13, ax14, ax15]):
+    ax.imshow(x_test_noised[random_images[i]].reshape(28, 28), cmap='gray')
+    if i ==0:
+        ax.set_ylabel("NOISE", size=20)
+        ax.grid(False)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        
+# 오코인코더가 출력한 이미지를 아래에 그린다.
+for i, ax in enumerate([ax6, ax7, ax8, ax9, ax10]):
+    ax.imshow(output[random_images[i]].reshape(28, 28), cmap='gray')
+    if i ==0:
+        ax.set_ylabel("OUTPUT", size=20)
+        ax.grid(False)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        
+plt.tight_layout()
+plt.show()
+
+    
+        
